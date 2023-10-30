1ec53cd69758cb4560ffcd5b82c097a06c70cfda
xMirnax <mirna.tominac@gmail.com>
2023-10-30 20:22:35 +0100

    Add project files.

---
+++ Algebra_seminarski_rad _Pokemon_webshop_CMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Algebra_seminarski_rad__Pokemon_webshop_CMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
