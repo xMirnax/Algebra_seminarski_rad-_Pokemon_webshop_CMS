925d402562f530357b758a4d03991396a3135cc6
xMirnax <mirna.tominac@gmail.com>
2023-11-02 18:06:55 +0100

    Added Toastr notifications

--- Algebra_seminarski_rad _Pokemon_webshop_CMS/Controllers/CategoryController.cs
+++ Algebra_seminarski_rad _Pokemon_webshop_CMS/Controllers/CategoryController.cs
@@ -16,5 +16,84 @@ namespace Algebra_seminarski_rad__Pokemon_webshop_CMS.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj= _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted";
+            return RedirectToAction("Index");
+
+        }
+
     }
 }
--- Algebra_seminarski_rad _Pokemon_webshop_CMS/Models/Category.cs
+++ Algebra_seminarski_rad _Pokemon_webshop_CMS/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Algebra_seminarski_rad__Pokemon_webshop_CMS.Models
 {
@@ -6,7 +7,11 @@ namespace Algebra_seminarski_rad__Pokemon_webshop_CMS.Models
     {
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100)]
         public int DisplayOrder { get; set; }
     }
 }
