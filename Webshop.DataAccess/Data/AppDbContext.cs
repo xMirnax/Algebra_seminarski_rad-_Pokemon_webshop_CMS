0ec89f3396d15b385682e91f40bd20707742aece
xMirnax <mirna.tominac@gmail.com>
2023-11-13 11:56:00 +0100

    Git reverted

---
+++ Webshop.DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Algebra_seminarski_rad__Pokemon_webshop_CMS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Webshop.DataAcess.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category {Id = 1, Name = "Fire", DisplayOrder = 1 },
+                new Category {Id = 2, Name = "Water", DisplayOrder = 2 },
+                new Category {Id = 3, Name = "Psychic", DisplayOrder = 3 }
+                );
+        }
+    }
+}
---
+++ Webshop.DataAccess/Migrations/20231031133102_CategoryTypeTable.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Algebra_seminarski_rad__Pokemon_webshop_CMS.DataAcess.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryTypeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Fire" },
+                    { 2, 2, "Water" },
+                    { 3, 3, "Psychic" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
