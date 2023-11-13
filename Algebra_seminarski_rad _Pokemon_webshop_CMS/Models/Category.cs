943e610466e64d542c8bc5ccdce339124441c050
xMirnax <mirna.tominac@gmail.com>
2023-11-13 12:08:38 +0100

    Revert "Added Toastr notifications"
    This reverts commit 925d402562f530357b758a4d03991396a3135cc6.

--- Algebra_seminarski_rad _Pokemon_webshop_CMS/Models/Category.cs
+++ Algebra_seminarski_rad _Pokemon_webshop_CMS/Models/Category.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Algebra_seminarski_rad__Pokemon_webshop_CMS.Models
 {
@@ -7,11 +6,7 @@ namespace Algebra_seminarski_rad__Pokemon_webshop_CMS.Models
     {
         public int Id { get; set; }
         [Required]
-        [MaxLength(30)]
-        [DisplayName("Category Name")]
         public string Name { get; set; }
-        [DisplayName("Display Order")]
-        [Range(1, 100)]
         public int DisplayOrder { get; set; }
     }
 }
